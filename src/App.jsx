e106f9f646e8789a7df8facce8049f70a45393bf
Timo <timo23075@gmail.com>
2025-06-01 15:42:44 +0200

    basic setup and design

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Die from './Die.jsx'
+
+function App() {
+
+  return (
+    <main>
+      <section className='numbers'>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+        <Die value='1'/>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Die.jsx
@@ -0,0 +1,12 @@
+function Die(props){
+
+   return (
+      <>
+         <button className="numberBtn">{props.value}</button>
+      </>
+   )
+
+
+}
+
+export default Die
