4ad3be40eadb4495d002880cbf96e69fa9ace83f
Timo <timo23075@gmail.com>
2025-06-02 21:15:09 +0200

    objects instead of arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Die from './Die.jsx'
 import {useState} from 'react'
+import { nanoid } from 'nanoid'
 
 function App() {
 
@@ -9,16 +10,16 @@ function App() {
     let arr = []
     for (let i = 0; i<10; i++){
       let num = Math.ceil(Math.random()*6);
-      arr.push(num)
+      arr.push({value: num, isHeld: false, id: nanoid()})
     }
     return arr;
   }
-
+  
 
   return (
     <main>
       <section className='numbers'>
-        {newDice.map(el => <Die value={el}/> )}
+        {newDice.map(el => <Die value={el.value} key={el.id}/> )}
       </section>
 
       <button onClick={() => setNewDice(generateAllNewDice())} className='rollBtn'>Roll</button>
